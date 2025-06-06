733f2b04affba40c008b5a872b5748109c99ca31
OrchNP <egorgrigoriev124@gmail.com>
2025-06-06 12:51:55 +0600

    add delete command

--- cmd/update.go
+++ cmd/update.go
@@ -16,30 +16,7 @@ var updateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "Update task",
 	Long:  `Update task`,
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) != 1 {
-			return errors.New("requiers one argument")
-		}
-
-		num, err := strconv.ParseInt(args[0], 10, 64)
-		elem = num
-		if err != nil {
-			return errors.New("argument must be a number")
-		}
-
-		if num < 0 {
-			return errors.New("number must be greater than 0")
-		}
-
-		openJSON("cmd/data.json", &tasks)
-
-		if len(tasks.Tasks) == 0 {
-			return errors.New("there are no tasks")
-		}
-
-		return nil
-
-	},
+	Args:  checkArgsInt,
 	Run: func(cmd *cobra.Command, args []string) {
 		if taskText != "" {
 			tasks.Tasks[elem].Text = taskText
@@ -56,4 +33,28 @@ func init() {
 	rootCmd.AddCommand(updateCmd)
 	updateCmd.Flags().StringVarP(&taskText, "text", "t", "", "Update task text")
 	updateCmd.Flags().StringVarP(&taskState, "state", "s", "", "Update task state")
+}
+
+func checkArgsInt(cmd *cobra.Command, args []string) error {
+	if len(args) != 1 {
+		return errors.New("requiers one argument")
+	}
+
+	num, err := strconv.ParseInt(args[0], 10, 64)
+	elem = num
+	if err != nil {
+		return errors.New("argument must be a number")
+	}
+
+	if num < 0 {
+		return errors.New("number must be greater than 0")
+	}
+
+	openJSON("cmd/data.json", &tasks)
+
+	if len(tasks.Tasks) == 0 {
+		return errors.New("there are no tasks")
+	}
+
+	return nil
 }
