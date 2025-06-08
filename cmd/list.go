32f8598d9feee62bedbdb475406eea9de6e47069
OrchNP <egorgrigoriev124@gmail.com>
2025-06-08 11:48:23 +0600

    add state show in list comand

--- cmd/list.go
+++ cmd/list.go
@@ -17,8 +17,27 @@ var listCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		openJSON("cmd/data.json", &tasks)
 
+		tdIsSet := cmd.Flags().Lookup("todos").Changed
+		pIsSet := cmd.Flags().Lookup("progress").Changed
+		dIsSet := cmd.Flags().Lookup("done").Changed
+
 		for i := 0; i < len(tasks.Tasks); i++ {
-			fmt.Println(tasks.Tasks[i].Text, " ", tasks.Tasks[i].State)
+
+			if tdIsSet && tasks.Tasks[i].State == "To Do" {
+				fmt.Println(tasks.Tasks[i].Text, " ", tasks.Tasks[i].State)
+			}
+
+			if pIsSet && tasks.Tasks[i].State == "In Progress" {
+				fmt.Println(tasks.Tasks[i].Text, " ", tasks.Tasks[i].State)
+			}
+
+			if dIsSet && tasks.Tasks[i].State == "Done" {
+				fmt.Println(tasks.Tasks[i].Text, " ", tasks.Tasks[i].State)
+			}
+
+			if !tdIsSet && !pIsSet && !dIsSet {
+				fmt.Println(tasks.Tasks[i].Text, " ", tasks.Tasks[i].State)
+			}
 		}
 
 	},
@@ -26,5 +45,8 @@ var listCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+	listCmd.Flags().BoolP("todos", "t", true, "List to do tasks")
+	listCmd.Flags().BoolP("progress", "p", true, "List in progress tasks")
+	listCmd.Flags().BoolP("done", "d", true, "List done tasks")
 
 }
